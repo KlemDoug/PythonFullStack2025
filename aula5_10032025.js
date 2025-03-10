7c13b3afbe1d8bb91ab5fb57eb22cb24ae320176
Douglas Klem <klemdoug@gmail.com>
2025-03-10 20:56:41 -0300

    att aula5 javascript

--- aula5_10032025.js
+++ aula5_10032025.js
@@ -67,7 +67,7 @@ console.log("x é igual a y?", x === y);
 let temCarteira = true;
 let idade = 18;
 let temCarro = false;
-let podeDirigir = idade >= 18 || temCarteira;
+let podeDirigir = idade >= 18 && temCarteira;
 console.log("Pode dirigir?", podeDirigir);
 console.log("Pode dirigir e tem carro?", podeDirigir && temCarro);
 
@@ -86,3 +86,45 @@ contador -= 2; //contador=contador-2
 contador *= 3; //contador=contador*3
 console.log("Valor final do contador:", contador);
 
+let a = 10;
+let b = 20;
+let c = 30;
+let media = (a + b + c) / 3;
+console.log("Média:", media);
+console.log("A média é maior que 15 e menor que 25?", media > 15 && media < 25);
+
+let nota = 75;
+if (nota >= 60) {
+    console.log("Aprovado");
+} else if (nota >= 40) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
+
+let diaDaSemana = 3;
+switch (diaDaSemana) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda");
+        break;
+    case 3:
+        console.log("Terça");
+        break;
+    case 4:
+        console.log("Quarta");
+        break;
+    case 5:
+        console.log("Quinta");
+        break;
+    case 6:
+        console.log("Sexta");
+        break;
+    case 7:
+        console.log("Sábado");
+        break;
+    default:
+        console.log("Dia inválido");
+}
