be247a46ef396afac1d9975f8d773e3baa95e90f
Douglas Klem <klemdoug@gmail.com>
2025-05-07 17:02:25 -0300

    Add files via upload

---
+++ UC3/Aula5/03.Atividade_em_Aula.py
@@ -0,0 +1,14 @@
+# Atividade Prática: Sistema de uma Biblioteca
+'''
+Contexto:
+Você foi contratado para desenvolver um sistema de gerenciamento de biblioteca usando POO em Python.
+O sistema deve modelar livros, usuários e empréstimos, com funcionalidades básicas de cadastro, consulta e operações.
+
+Requisitos:
+- O sistema deve permitir o cadastro de livros, usuários e empréstimos.
+- O sistema deve permitir a consulta de livros cadastrados.
+- O sistema deve permitir a consulta de usuários cadastrados.
+'''
+
+# DESAFIO (opcional) Classe LivroDigital:
+# Herde de Livro e adicione o formato do e-book e formas de download.
